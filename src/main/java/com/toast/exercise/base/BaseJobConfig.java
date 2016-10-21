e6425d07f7d2961c15b6e46a458743f4877f2d78
NHNEnt siwoo <sw.talk@nhnent.com>
2016-10-21 11:48:10 +0900

    BaseJobConfig 파일 커밋 추가 #2016-10-21

--- src/main/java/com/toast/exercise/base/BaseJobConfig.java
+++ src/main/java/com/toast/exercise/base/BaseJobConfig.java
@@ -11,7 +11,7 @@ import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
-
+// Test commit source tree
 public class BaseJobConfig {
 	
 	@Autowired
